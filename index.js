02379ae4679e02dad4c9d0c4c3f32d0c750ae9a6
kissilajesus2 <139083402+kissilajesus2@users.noreply.github.com>
2023-08-05 19:33:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const num1 = prompt(`Digite o primeiro numero`)
+
+const num2 = prompt(`Digite o segundo numero`)
+
+window.alert("o resultado é" + Math.imul(num1,num2))
+
+const num = prompt("digite o valor principal")
+const exp = prompt("digite o expoente")
+
+alert("o resultado é" + Math.pow(num,exp))
+
+var nome = prompt("digite seu nome")
+var anoNasc = parseFloat(prompt("Digite seu ano de nascimento"))
+
+alert(`Òlá, seu nome é ${nome} e você tem` + (2023 - anoNasc + "anos"))
